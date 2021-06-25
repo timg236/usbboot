02c07823dae1406b720a7273c684fa44da4baf71
Tim Gover <tim.gover@raspberrypi.com>
2021-06-25 14:51:50 +0100

    Error if -k is specified but the file does not exist
    Update spacing in update-pieeprom.sh

--- tools/update-pieeprom.sh
+++ tools/update-pieeprom.sh
@@ -26,7 +26,7 @@ cleanup() {
 }
 
 usage() {
-cat <<EOF
+   cat <<EOF
     update-pieeprom.sh [key]
 
     Updates and EEPROM image by replacing the configuration file and generates
@@ -51,41 +51,41 @@ EOF
 }
 
 update_eeprom() {
-    src_image="$1"
-    config="$2"
-    dst_image="$3"
-    pem_file="$4"
-    sign_args=""
+   src_image="$1"
+   config="$2"
+   dst_image="$3"
+   pem_file="$4"
+   sign_args=""
 
-    if [ -n "${pem_file}" ]; then
-        if ! grep -q "SIGNED_BOOT=1" "${CONFIG}"; then
-            # If the OTP bit to require secure boot are set then then
-            # SIGNED_BOOT=1 is implicitly set in the EEPROM config.
-            # For debug in signed-boot mode it's normally useful to set this
-            echo "Warning: SIGNED_BOOT=1 not found in \"${CONFIG}\""
-        fi
-        TMP_CONFIG_SIG="$(mktemp)"
-        echo "Signing bootloader config"
-        "${script_dir}/rpi-eeprom-digest" \
-            -i "${config}" -o "${TMP_CONFIG_SIG}" \
-            -k "${pem_file}" || die "Failed to sign EEPROM config"
+   if [ -n "${pem_file}" ]; then
+      if ! grep -q "SIGNED_BOOT=1" "${CONFIG}"; then
+         # If the OTP bit to require secure boot are set then then
+         # SIGNED_BOOT=1 is implicitly set in the EEPROM config.
+         # For debug in signed-boot mode it's normally useful to set this
+         echo "Warning: SIGNED_BOOT=1 not found in \"${CONFIG}\""
+      fi
+      TMP_CONFIG_SIG="$(mktemp)"
+      echo "Signing bootloader config"
+      "${script_dir}/rpi-eeprom-digest" \
+         -i "${config}" -o "${TMP_CONFIG_SIG}" \
+         -k "${pem_file}" || die "Failed to sign EEPROM config"
 
-        cat "${TMP_CONFIG_SIG}"
+      cat "${TMP_CONFIG_SIG}"
 
         # rpi-eeprom-config extracts the public key args from the specified
         # PEM file. It will also accept just the public key so it's possible
         # to tweak this script so that rpi-eeprom-config never sees the private
         # key.
         sign_args="-d ${TMP_CONFIG_SIG} -p ${pem_file}"
-    fi
+      fi
 
-    rm -f "${dst_image}"
-    ${script_dir}/rpi-eeprom-config \
-        --config "${config}" \
-        --out "${dst_image}" ${sign_args} \
-        "${src_image}" || die "Failed to update EEPROM image"
+      rm -f "${dst_image}"
+      ${script_dir}/rpi-eeprom-config \
+         --config "${config}" \
+         --out "${dst_image}" ${sign_args} \
+         "${src_image}" || die "Failed to update EEPROM image"
 
-cat <<EOF
+      cat <<EOF
 new-image: ${dst_image}
 source-image: ${src_image}
 config: ${config}
@@ -93,34 +93,35 @@ EOF
 }
 
 image_digest() {
-    "${script_dir}/rpi-eeprom-digest" \
-        -i "${1}" -o "${2}"
-}
+   "${script_dir}/rpi-eeprom-digest" \
+      -i "${1}" -o "${2}"
+   }
 
 trap cleanup EXIT
 
 while getopts "c:hi:o:k:" option; do
-    case "${option}" in
-        c) CONFIG="${OPTARG}"
-            ;;
-        i) SRC_IMAGE="${OPTARG}"
-            ;;
-        o) DST_IMAGE="${OPTARG}"
-            ;;
-        k) PEM_FILE="${OPTARG}"
-            ;;
-        h) usage
-            ;;
-        *) echo "Unknown argument \"${option}\""
-            usage
-            ;;
-    esac
+   case "${option}" in
+      c) CONFIG="${OPTARG}"
+         ;;
+      i) SRC_IMAGE="${OPTARG}"
+         ;;
+      o) DST_IMAGE="${OPTARG}"
+         ;;
+      k) PEM_FILE="${OPTARG}"
+         [ -f "${KEY}" ] || echo "Missing private key file \"${KEY}\""
+         ;;
+      h) usage
+         ;;
+      *) echo "Unknown argument \"${option}\""
+         usage
+         ;;
+   esac
 done
 
 [ -f "${SRC_IMAGE}" ] || die "Source image \"${SRC_IMAGE}\" not found"
 [ -f "${CONFIG}" ] || die "Bootloader config file \"${CONFIG}\" not found"
 if [ -n "${PEM_FILE}" ]; then
-    [ -f "${PEM_FILE}" ] || die "RSA key file \"${PEM_FILE}\" not found"
+   [ -f "${PEM_FILE}" ] || die "RSA key file \"${PEM_FILE}\" not found"
 fi
 
 DST_IMAGE_SIG="$(echo "${DST_IMAGE}" | sed 's/\..*//').sig"
